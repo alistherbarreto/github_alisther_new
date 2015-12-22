0dcb4c635182f00f076f4a7f1cefdc0e8e937b20
Jude <jude.manutd@gmail.com>
2015-12-22 10:49:47 +0530

    test cases

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,14 @@ public class Client {
 
 	public static void main(String[] args) {
 		QueueProcess qp = new QueueProcess();
+		System.out.println(qp.disp());
+		qp.add(5);
+		qp.add(6);
+		qp.add("test");
+		qp.add(8);
+		System.out.println(qp.disp());
+		System.out.println(qp.remove());
+		System.out.println(qp.disp());
 	}
 
 }
