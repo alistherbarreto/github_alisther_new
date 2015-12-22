3185682693588134340c9c13c8a70fd68843c3c9
Jude <jude.manutd@gmail.com>
2015-12-22 13:14:16 +0530

    Final array queue

--- src/Client.java
+++ src/Client.java
@@ -2,15 +2,42 @@
 public class Client {
 
 	public static void main(String[] args) {
-		QueueProcess qp = new QueueProcess();
-		System.out.println(qp.disp());
-		qp.add(5);
-		qp.add(6);
-		qp.add("test");
-		qp.add(8);
-		System.out.println(qp.disp());
-		System.out.println(qp.remove());
-		System.out.println(qp.disp());
+
+		/*****
+		 * Queue Using arrayList
+		 */
+		System.out.println("ArrayList");
+
+		QueueProcessArrayList queueList = new QueueProcessArrayList();
+		queueList.add(5);
+		queueList.add(6);
+		queueList.add("Hello World");
+		queueList.add(8);
+		System.out.println(queueList.disp());
+		System.out.println(queueList.remove());
+		System.out.println(queueList.disp());
+
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		/*****
+		 * Queue Using arrays
+		 */
+		System.out.println("Arrays");
+		QueueProcessArrays queueArray = new QueueProcessArrays(3);
+		queueArray.disp();
+		System.out.println(queueArray.add(5));
+		System.out.println(queueArray.add(6));
+		System.out.println(queueArray.add("test"));
+		System.out.println(queueArray.add(8));
+		queueArray.disp();
+
+		System.out.println(queueArray.remove());
+		System.out.println(queueArray.remove());
+		System.out.println(queueArray.remove());
+		System.out.println(queueArray.remove());
+		queueArray.disp();
 	}
 
 }
